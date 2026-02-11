f8a39a38e0c40e700d0f036c8274df559fad355a
Anne Jankovics <a.jankovics@dreistrom.land>
2026-02-11 14:04:16 +0100

    [TASK] #DEVOPS-1064 Optimize the ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,26 +1,26 @@
 <?php
 
-defined('TYPO3') or die();
+declare(strict_types=1);
 
-call_user_func(static function (): void {
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+defined('TYPO3') || die();
+
+(static function (): void {
+    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
     $iconRegistry->registerIcon(
         'turnstile',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        SvgIconProvider::class,
         [
             'source' => 'EXT:turnstile/Resources/Public/Icons/turnstile.svg',
         ]
     );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-        'turnstile',
-        'setup',
-        'module.tx_form {
-          settings {
-            yamlConfigurations {
-              1692719161 = EXT:turnstile/Configuration/Form/Yaml/BaseSetup.yaml
-            }
-          }
-        }'
-    );
-});
+    ExtensionManagementUtility::addTypoScriptSetup('
+        module.tx_form.settings.yamlConfigurations.1692719161 = EXT:turnstile/Configuration/Form/Yaml/BaseSetup.yaml
+        plugin.tx_form.settings.yamlConfigurations.1692719161 = EXT:turnstile/Configuration/Form/Yaml/BaseSetup.yaml
+    ');
+})();
