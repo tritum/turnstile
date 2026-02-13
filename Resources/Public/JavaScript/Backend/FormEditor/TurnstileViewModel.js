a978ec804865931c3a02031d9e78e2fe77db504c
Anne Jankovics <a.jankovics@dreistrom.land>
2026-02-13 05:57:03 +0100

    [TASK] #DEVOPS-1064 Remove outdated RequireJS

--- Resources/Public/JavaScript/Backend/FormEditor/TurnstileViewModel.js
+++ Resources/Public/JavaScript/Backend/FormEditor/TurnstileViewModel.js
@@ -1,109 +1,38 @@
 /**
- * Module: TYPO3/CMS/Turnstile/Backend/FormEditor/TurnstileViewModel
+ * Module: @tritum/turnstile/Backend/FormEditor/TurnstileViewModel.js
  */
-define([
-    'jquery',
-    'TYPO3/CMS/Form/Backend/FormEditor/Helper'
-], function ($, Helper) {
-    'use strict';
+import $ from 'jquery';
+import * as Helper from '@typo3/form/backend/form-editor/helper.js';
 
-    return (function ($, Helper) {
-        /**
-         * @private
-         *
-         * @var object
-         */
-        var _formEditorApp = null;
+let formEditorApp = null;
 
-        /**
-         * @private
-         *
-         * @return object
-         */
-        function getFormEditorApp() {
-            return _formEditorApp;
+function getPublisherSubscriber() {
+    return formEditorApp.getPublisherSubscriber();
+}
+
+function assert(test, message, messageCode) {
+    return formEditorApp.assert(test, message, messageCode);
+}
+
+function helperSetup() {
+    assert(
+        typeof Helper.bootstrap === 'function',
+        'The view model helper does not implement the method "bootstrap"',
+        1491643380
+    );
+    Helper.bootstrap(formEditorApp);
+}
+
+function subscribeEvents() {
+    getPublisherSubscriber().subscribe('view/stage/abstract/render/template/perform', (topic, args) => {
+        if (args[0].get('type') === 'Turnstile') {
+            formEditorApp.getViewModel().getStage().renderSimpleTemplateWithValidators(args[0], args[1]);
         }
+    });
+}
 
-        /**
-         * @private
-         *
-         * @return object
-         */
-        function getPublisherSubscriber() {
-            return getFormEditorApp().getPublisherSubscriber();
-        }
-
-        /**
-         * @private
-         *
-         * @param {boolean} test
-         * @param {string} message
-         * @param {int} messageCode
-         *
-         * @return void
-         */
-        function assert(test, message, messageCode) {
-            return getFormEditorApp().assert(test, message, messageCode);
-        }
-
-        /**
-         * @private
-         *
-         * @return void
-         *
-         * @throws 1491643380
-         */
-        function _helperSetup() {
-            assert(
-                'function' === $.type(Helper.bootstrap),
-                'The view model helper does not implement the method "bootstrap"',
-                1491643380
-            );
-            Helper.bootstrap(getFormEditorApp());
-        }
-
-        /**
-         * @private
-         *
-         * @return void
-         */
-        function _subscribeEvents() {
-            /**
-             * @private
-             *
-             * @param string
-             * @param array
-             *              args[0] = formElement
-             *              args[1] = template
-             *
-             * @return void
-             */
-            getPublisherSubscriber().subscribe('view/stage/abstract/render/template/perform', function (topic, args) {
-                if (args[0].get('type') === 'Turnstile') {
-                    getFormEditorApp().getViewModel().getStage().renderSimpleTemplateWithValidators(args[0], args[1]);
-                }
-            });
-        }
-
-        /**
-         * @public
-         *
-         * @param {object} formEditorApp
-         *
-         * @return void
-         */
-        function bootstrap(formEditorApp) {
-            _formEditorApp = formEditorApp;
-            _helperSetup();
-            _subscribeEvents();
-        }
-
-        /**
-         * Publish the public methods.
-         * Implements the "Revealing Module Pattern".
-         */
-        return {
-            bootstrap: bootstrap
-        };
-    })($, Helper);
-});
+export function bootstrap(app) {
+    formEditorApp = app;
+    helperSetup();
+    subscribeEvents();
+}
